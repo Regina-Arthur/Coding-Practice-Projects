9349d69f41e1d8c25c7a31cc8bcab76b571f2615
Regina Arthur <reginaeditharthur@gmail.com>
2026-06-23 23:04:58 +0000

    building my expense class for my expense tracker

---
+++ Playing_with_code/expense.py
@@ -0,0 +1,17 @@
+class expense():
+    def __init__(self, total_amount):
+        self.total_amount = total_amount
+        
+
+
+    def name(self):
+
+
+
+
+    def category(self):
+
+
+
+
+    def amount(self):
