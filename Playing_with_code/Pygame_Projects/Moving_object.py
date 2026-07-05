6ab5ccb64b7380f7f96446adad7aedd909d5638c
Regina Arthur <reginaeditharthur@gmail.com>
2026-07-05 20:05:51 +0000

    Added some documentation to my code

--- Playing_with_code/Pygame_Projects/Moving_object.py
+++ Playing_with_code/Pygame_Projects/Moving_object.py
@@ -1,16 +1,16 @@
 import pygame
 
-# pygame setup
+#pygame setup
 pygame.init()
 SCREEN_WIDTH = 673
 SCREEN_HEIGHT = 673
 CHARACTER_SIZE = 80
 
-# Character position variables
+#Character position variables
 player_x = 375
 player_y = 275
 
-# Movement speed (pixels per frame)
+#Movement speed (pixels per frame)
 player_speed = 5
 
 pygame.display.set_caption("Yummy Chocolate Game")
@@ -21,51 +21,51 @@ character = pygame.transform.scale(pygame.image.load("images-removebg-preview.pn
 running = True
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    #poll for events
+    pygame.QUIT #event means the user clicked X to close your window
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+         if event.type == pygame.QUIT:
+             running = False
             
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        player_x -= player_speed
+         player_x -= player_speed
     if keys[pygame.K_RIGHT]:
-        player_x += player_speed
+         player_x += player_speed
     if keys[pygame.K_UP]:
-        player_y -= player_speed
+         player_y -= player_speed
     if keys[pygame.K_DOWN]:
-        player_y += player_speed
+         player_y += player_speed
 
 
     #Apply Boundary Restrictions (The Clamping)
-    # Left boundary
+    #Left boundary
     if player_x < 0:
-        player_x = 0
+         player_x = 0
 
-    # Right boundary (Screen width minus character width)
+    #Right boundary (Screen widt minus character width)
     if player_x > SCREEN_WIDTH - CHARACTER_SIZE:
-        player_x = SCREEN_WIDTH - CHARACTER_SIZE
+         player_x = SCREEN_WIDTH - CHARACTER_SIZE
 
-    # Top boundary
+    #Top boundary
     if player_y < 0:
-        player_y = 0
+         player_y = 0
 
-    # Bottom boundary (Screen height minus character height)
+    #Bottom boundary (Screen height minus character height)
     if player_y > SCREEN_HEIGHT - CHARACTER_SIZE:
-        player_y = SCREEN_HEIGHT - CHARACTER_SIZE
+         player_y = SCREEN_HEIGHT - CHARACTER_SIZE
 
-    # fill the screen with a color to wipe away anything from last frame
+    #fill the screen with a color to wipe away anything from last frame
     screen.blit(background,(0,0))
     screen.blit(character, (player_x, player_y))
 
 
 
-    # RENDER YOUR GAME HERE
+    #RENDER YOUR GAME HERE
 
-    # flip() the display to put your work on screen
+    #flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(60)   #limits FPS to 60
 
 pygame.quit()
