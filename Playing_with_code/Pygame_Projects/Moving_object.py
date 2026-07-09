26ad6600075db01ff5fecc25c1ba4c9eba854426
Regina Arthur <reginaeditharthur@gmail.com>
2026-07-09 23:27:00 +0000

    updated documentation

--- Playing_with_code/Pygame_Projects/Moving_object.py
+++ Playing_with_code/Pygame_Projects/Moving_object.py
@@ -22,7 +22,7 @@ running = True
 
 while running:
     #poll for events
-    pygame.QUIT #event means the user clicked X to close your window
+    #pygame.QUIT #event means the user clicked X to close your window
     for event in pygame.event.get():
          if event.type == pygame.QUIT:
              running = False
